d5bcc7334b503ad3e183bd4e487b4d519739fdc9
mariaomarsd <42963245+mariaomarsd@users.noreply.github.com>
2022-04-04 01:37:10 +0200

    move app code and create correct app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,19 @@
-import React from "react";
-import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
-/*import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import { formatRelative } from "date-fns";
-*/
-//import "@reach/combobox/styles.css";
-import mapStyles from "./mapStyles";
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-const libraries = ["places"];
-const mapContainerStyle = {
-  height: "100vh",
-  width: "100vw",
-};
-
-const center = { // where to start the map, stockholm
-  lat: 59.327607,
-  lng: 18.064266,
-};
-const options = {
-  styles: mapStyles,
-  disableDefaultUI: true,
-  zoomControl: true,
-};
+//tjekka hvort virki, breyta svo í presenter
+const Map = require("./views/mapView.js").default;
+const NewTrip = require("./views/newTripView.js").default;
 
 export default function App() {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries,
-  });
-
-  if (loadError) return "Error";
-  if (!isLoaded) return "Loading...";
-
   return (
-    <div>
-      <GoogleMap id="map"
-        mapContainerStyle={mapContainerStyle}
-        zoom={8}
-        center={center}
-        options={options}
-        
-      ></GoogleMap>
+    <div class="app">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Map/>} />
+          <Route exact path="/" element={<NewTrip/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
