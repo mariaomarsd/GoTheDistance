d984c8c1b406eb29dcb1de9f893b5f1a09fe7517
mariaomarsd <42963245+mariaomarsd@users.noreply.github.com>
2022-04-04 00:53:28 +0200

    folder setup

---
+++ client/src/views/newTripView.js
@@ -0,0 +1,14 @@
+
+function newTripView(props){
+
+    return(
+        <div class="new-trip-view">
+            <div class="search-place">
+                <input placeholder="Search a place!" />
+                <button > Add to trip </button>
+            </div>
+        </div>
+    );
+}
+
+export default newTripView;
