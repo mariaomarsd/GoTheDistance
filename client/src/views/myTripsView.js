3318ae225a774fb21e53abab680242eea519ff17
mariaomarsd <42963245+mariaomarsd@users.noreply.github.com>
2022-04-05 16:50:35 +0200

    my trips added

---
+++ client/src/views/myTripsView.js
@@ -0,0 +1,65 @@
+import React from 'react';
+// import { Checkbox } from 'antd';
+import '../styles/myTripView.css';
+
+function MyTripsView() {
+
+    const myTrips = [
+    {
+        name: "New York",
+        isChecked: false
+    },
+    {
+        name: "London",
+        isChecked: false
+    },
+    {
+        name: "Texas",
+        isChecked: false
+    },
+    {
+        name:"Chicago",
+        isChecked: false
+    }]
+
+  return ( 
+     <div className="my-trips-view">
+        {myTrips.map((items) => 
+        <div className="my-trips-view-box">
+            <div className="my-trip-checkbox">
+                {/* <Checkbox></Checkbox> */}
+                <input type="checkbox"></input>
+            </div>
+            <div className="my-trip-name">
+                {items.name}
+            </div>
+        </div>)}
+    </div>
+  );
+}
+
+// return (
+//     <div>
+//         <div className="main-div">
+//             {insurances.map((item) => (
+//                 <div
+//                     key={item.id}
+//                     className={item.isChecked ? 'choice-border-dark' : 'choice-border-light'}
+//                 >
+//                     <Paragraph
+//                         className={item.isChecked ? 'text-choice-dark' : 'text-choice-light'}
+//                         onClick={() => { onCheck(item.id); }}
+//                     >
+//                         {item.title}
+//                         <Checkbox
+//                             className="check-box"
+//                             checked={item.isChecked}
+//                         />
+//                     </Paragraph>
+//                 </div>
+//             ))}
+//         </div>
+//     </div>
+// );
+
+export default MyTripsView;
