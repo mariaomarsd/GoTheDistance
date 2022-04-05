003ff312315df24b5cde45d494a17a0b15ad5cc8
OlafurKonrad <40210174+OlafurKonrad@users.noreply.github.com>
2022-04-05 16:26:53 +0200

    search bar

--- client/src/views/mapView.js
+++ client/src/views/mapView.js
@@ -1,23 +1,28 @@
-import React from "react";
+//import React from "react";
+import React, { Component } from "react";
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-/*import usePlacesAutocomplete, {
+import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
-import {
+
+///*
+  import { // just for box looks
   Combobox,
   ComboboxInput,
   ComboboxPopover,
   ComboboxList,
   ComboboxOption,
 } from "@reach/combobox";
-import { formatRelative } from "date-fns";
-*/
+//*/
+import "@reach/combobox/styles.css";
+//import { formatRelative } from "date-fns";
+
 //import "@reach/combobox/styles.css";
 import mapStyles from "../mapStyles";
 
@@ -48,6 +53,7 @@ export default function App() {
 
   return (
     <div>
+      <Search  />
       <GoogleMap id="map"
         mapContainerStyle={mapContainerStyle}
         zoom={8}
@@ -58,4 +64,52 @@ export default function App() {
     </div>
   );
 }
+function Search() {
+  const {
+    ready, // is it set up and redy to go with libraries, see above  in app function
+    value, // what is the current value that user is writing
+    suggestions: { status, data }, // what is the data from these suggestions
+    setValue, 
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: { // preffer places that are near this location 
+    
+    },
+  });
 
+  // "onSelect={(adress)=>" refers to when option chosen from drop down
+  // "onChange={(e) => { setValue(e.target.value);" refers to user input
+  // " "
+
+  return (
+    <div className="Search">
+      <Combobox 
+        onSelect={async (address)=> {
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            console.log({ lat, lng });
+          } catch (error) {
+            //console.log(" Error: ", error);
+          }
+          console.log(address);
+        }}>
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Search your location"
+        />
+        <ComboboxPopover>
+        {status === "OK" &&
+              data.map(({ id, description, }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+        </ComboboxPopover>
+        
+      </Combobox>
+    </div>
+  );
+}
