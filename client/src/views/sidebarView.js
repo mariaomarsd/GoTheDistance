e69578ba1623f4a310bc772ccac93ff27219a1a7
hallbjorgembla <hes85@hi.is>
2022-04-30 22:47:46 +0200

    can create users

--- client/src/views/sidebarView.js
+++ client/src/views/sidebarView.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SignUpPresenter from "../presenters/signUpPresenter.js";
 
 const NewTripPresenter = require("../presenters/newTripPresenter.js").default;
 const MyTripsPresenter = require("../presenters/myTripsPresenter.js").default;
@@ -17,6 +18,9 @@ function SidebarView(props) {
                     // setVisableTrips={props.model.setVisableTrips}
                     model={props.model}
                />
+           </div>
+           <div>
+               <SignUpPresenter model = {props.model}/>
            </div>
         </div>
     );
